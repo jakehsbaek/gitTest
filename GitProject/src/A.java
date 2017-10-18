ee981779321cc3a93d4c8059e0d4eb5492b9d664
pc-16 <pc-16@pc-16-PC>
2017-10-18 14:08:32 +0900

    생성자추가

--- GitProject/src/A.java
+++ GitProject/src/A.java
@@ -1,4 +1,8 @@
 
 public class A {
+	
+	public A(){
+		
+	}
 
 }
--- GitProject/src/Main.java
+++ GitProject/src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("출력");
-
+        System.out.println("추가");
+        
 	}
 
 }
